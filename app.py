0f342a566d505d4d0af6dcba0eab004e0fff7805
Abodsp66 <65359563+Abodsp66@users.noreply.github.com>
2024-05-03 16:28:24 +0300

    app.py

--- app.py
+++ app.py
@@ -1,40 +1,11 @@
-import requests
-from flask import Flask, request
+import telebot
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = "7077125494:AAEfbQ6xjGvyz44aAy2fPVAS_yQFGgmwS44"
+bot = telebot.TeleBot("6782521207:AAGojdYUCbSzfGjVBVF87xTrpJZqWbjbX7Q")
+@bot.message_handler()
+def Myfunc(message):
+    bot.send_message(message.chat.id, "Hi, What's happend?")
 
-# Replace 'YOUR_WEBHOOK_URL' with your webhook URL provided by Telegram or your hosting provider
-WEBHOOK_URL = "https://arbitrage-2smi.onrender.com"
 
-app = Flask(__name__)
 
-def send_message(chat_id, text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    params = {"chat_id": chat_id, "text": text}
-    response = requests.get(url, params=params)
-    return response.json()
 
-def set_webhook():
-    url = f"https://api.telegram.org/bot{TOKEN}/setWebhook"
-    params = {"url": WEBHOOK_URL}
-    response = requests.get(url, params=params)
-    return response.json()
-
-@app.route('/', methods=['POST'])
-def handle_message():
-    data = request.json
-    chat_id = data["message"]["chat"]["id"]
-    text = data.get("message", {}).get("text", "")
-
-    if "/start" in text:
-        send_message(chat_id, "Hello!")
-
-    return {"status": "OK"}
-
-if __name__ == "__main__":
-    # Set the webhook URL
-    set_webhook()
-
-    # Run the Flask app
-    app.run(debug=True)
+bot.infinity_polling(skip_pending=True)
